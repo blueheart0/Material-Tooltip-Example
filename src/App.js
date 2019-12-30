72aa9092dd59e2c750b0fcd036343fa2c333be5a
SeungHyun Kim <k10130219@gmail.com>
2019-12-30 11:55:16 +0900

    Modify tooltip message
    1. Modify tooltip message to component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
 import { Box, createMuiTheme } from "@material-ui/core";
 import Grid from "@material-ui/core/Grid";
 import ThemeProvider from "@material-ui/styles/ThemeProvider";
-import React, { useContext } from "react";
+import React, { forwardRef, useContext } from "react";
 import "./App.css";
 import CCTooltip from "./Component";
 import { AppContext } from "./Context/AppContext";
 import logo from "./logo.svg";
 
-function App() {
+const Message = forwardRef((props, ref) => {
+  return (
+    <Box>
+      <Grid container direction={"column"}>
+        <Grid item>김가나 (e4501230534)</Grid>
+        <Grid item>01045436456</Grid>
+        <Grid item>아이싱</Grid>
+        <Grid item>금일 필히 아이싱!!!</Grid>
+      </Grid>
+    </Box>
+  );
+});
+
+const App = () => {
   return (
     <div className="App">
       <Box>
         <Grid container justify={"center"}>
           <Grid item>
-            <CCTooltip
-              open={true}
-              title={<span style={{ color: "red" }}> TEST TOOLTIP!!!</span>}
-            >
+            <CCTooltip title={<Message />}>
               <img src={logo} width={200} height={200} alt={""} />
             </CCTooltip>
           </Grid>
@@ -24,7 +34,7 @@ function App() {
       </Box>
     </div>
   );
-}
+};
 const WrapApp = props => {
   const { appContext } = useContext(AppContext);
   console.log(appContext);
